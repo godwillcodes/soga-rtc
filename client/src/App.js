205598199ce8db4dc68aba098444c003b2b0964d
GodwillB <iamgodwillb@gmail.com>
2022-08-10 05:27:08 +0300

    client-side nesting components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
+import { Typography, AppBar } from "@material-ui/core";
 
-const app = () => {
+import VideoPlayer from "./components/VideoPlayer";
+import Options from "./components/Options";
+import Notifications from "./components/Notifications";
+
+const App = () => {
     return (
         <div>
-            <h1>Hello World</h1>
+            <AppBar position="static" color="inherit">  
+                <Typography variant="h6" align="center">Hello World</Typography>
+            </AppBar>
+            <VideoPlayer/>
+            <Options>
+                <Notifications/>
+            </Options>
         </div>
     );
 }
 
-export default app;
+export default App;
+
+
+
---
+++ client/src/components/VideoPlayer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const VideoPlayer = () => {
+    return (
+        <div>
+          <h3>VideoPlayer</h3>
+          
+        </div>
+    );
+}
+
+export default VideoPlayer;
