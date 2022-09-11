ac2ec012d3835580a7ee8adb12049ee78535ae09
GodwillB <iamgodwillb@gmail.com>
2022-09-11 07:22:43 +0300

    initial setup

--- index.js
+++ index.js
@@ -1,35 +1,7 @@
-const app = require("express")();
-const server = require("http").createServer(app);
-const cors = require("cors");
+let localStream;
+let remoteStream;
 
-const io = require ("socket.io")(server, {
-    cors : {
-        origin : "*",
-        methods : ["GET", "POST"],
-    }
-});
-
-app.use(cors());
-
-const port = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-    res.send("Hello World");
-});
-
-io.on("connection", (socket) => {
-    socket.emit('me', socket.id);
-    socket.on("disconnect", () => {
-        socket.broadcast.emit("user-disconnected", socket.id);
-    } );
-
-    socket.on("calluser", ({userToCall, signalData, from, name}) => {
-        io.to(userToCall).emit("calluser", {signalData, from, name});
-    } );
-
-    socket.on("accept-call", ({signalData, from, name}) => {    
-        io.to(from).emit("accept-call", {signalData, from, name});
-    } );
-});
-
-server.listen(port, () => console.log(`Server is running on port ${port}`));
+let init = async () => {    
+    localStream = await navigator.mediaDevices.getUserMedia({video: true, audio: true});
+    document.getElementById('local').srcObject = localStream;
+}
